ff6970d125bc44f0cd82991a75217ea9e47e84a2
Alex Warner <alex@overtureweb.com>
2021-11-03 14:57:34 -0700

    Bug fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
 const path = require('path');
 
 module.exports = (env) => {
-	return {
-		mode: env.mode || "development",
-		entry: './src/index.ts',
-		devtool: 'inline-source-map',
-		devServer: {
-			static: './dist',
-			port: 3000
-		},
-		output: {
-			filename: 'bundle.js',
-			path: path.resolve(__dirname, 'dist'),
-		},
-		module: {
-			rules: [
-				{
-					test: /\.tsx?$/,
-					use: 'ts-loader',
-					exclude: /node_modules/,
-				},
-				{
-					test: /\.s[ac]ss$/i,
-					use: ["css-loader", "sass-loader"],
-				},
-				{
-					test: /\.html$/i,
-					loader: "html-loader",
-				},
-			],
-		},
-		resolve: {
-			extensions: ['.tsx', '.ts', '.js'],
-		},
-	}
+    return {
+        mode: env.mode || "development",
+        entry: './src/index.ts',
+        devtool: 'inline-source-map' && env.mode !== "production",
+        devServer: {
+            static: './dist',
+            port: 3000
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: ["css-loader", "sass-loader"],
+                },
+                {
+                    test: /\.html$/i,
+                    loader: "html-loader",
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+    }
 };
